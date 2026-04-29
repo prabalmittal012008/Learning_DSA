a12fc39b691ee2af207daa2d93306dc6187e742a
prabalmittal012008 <prabalmittal012008@gmail.com>
2026-04-29 23:30:32 +0530

    Add files via upload

---
+++ Day-86.c
@@ -0,0 +1,19 @@
+int mySqrt(int x) {
+    if(x == 0) return 0;
+
+    int low = 1, high = x;
+    int ans = 0;
+
+    while(low <= high) {
+        long long mid = low + (high - low) / 2;
+
+        if(mid * mid <= (long long)x) {
+            ans = (int)mid;
+            low = (int)mid + 1;
+        } else {
+            high = (int)mid - 1;
+        }
+    }
+
+    return ans;
+}
